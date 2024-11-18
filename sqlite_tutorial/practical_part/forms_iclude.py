3829db3b0cafcb9ca7d4bb4f42fcd80e9bef3d36
dima cheban <dimacheban23@gmail.com>
2024-11-18 20:47:35 +0200

    Add forms for student and teacher contacts
    Extended the forms_include.py script to include SQL insertion templates for ContactsStudent and ContactsTeacher tables. This allows for capturing email and phone data for both students and teachers.

--- sqlite_tutorial/practical_part/forms_iclude.py
+++ sqlite_tutorial/practical_part/forms_iclude.py
@@ -5,3 +5,6 @@ form_students = 'INSERT INTO Students (firstname, lastname, enrollment, group_id
 form_teachers = 'INSERT INTO Teachers (firstname, lastname) VALUES (?, ?)'
 form_subject = 'INSERT INTO Subjects (name, teacher_id) VALUES (?, ?)'
 form_grade = 'INSERT INTO Grades ( student_id, subject_id, grade, date_registered ) VALUES (?, ?, ?, ?)'
+form_contact_student = 'INSERT INTO ContactsStudent(student_id, student_email, student_phone) VALUES (?, ?, ?)'
+form_contact_teacher = 'INSERT INTO ContactsTeacher(teacher_id, teacher_email, teacher_phone) VALUES (?, ?, ?)'
+
