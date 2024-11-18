651539aeb7e83aef47a9a80ecd7018a2063161ac
dima cheban <dimacheban23@gmail.com>
2024-11-18 17:14:10 +0200

    Add functionality for filling grades in the database
    Implemented new functions to generate random grades, dates, and retrieve student and subject IDs. Updated the main script to populate the Grades table with random data.

--- sqlite_tutorial/practical_part/fill_tables.py
+++ sqlite_tutorial/practical_part/fill_tables.py
@@ -17,12 +17,19 @@ def create_random_teacher()->tuple[str,str]:
     last_name = faker.last_name()
     return first_name, last_name
 
+def create_random_grade()->int:
+    return faker.random_int(0, 100)
+
+def create_random_data():
+    return faker.date(pattern='%Y-%m-%d')
+
 name_groups = ['Group-A', 'Group-B', 'Group-C']
 form_query = 'INSERT INTO Groups (name_group) VALUES (?)'
 
 form_students = 'INSERT INTO Students (firstname, lastname, enrollment, group_id) VALUES (?, ?, ?, ?)'
 form_teachers = 'INSERT INTO Teachers (firstname, lastname) VALUES (?, ?)'
 form_subject = 'INSERT INTO Subjects (name, teacher_id) VALUES (?, ?)'
+form_grade = 'INSERT INTO Grades ( student_id, subject_id, grade, date_registered ) VALUES (?, ?, ?, ?)'
 
 def fill_table_groups(c:Cursor,sql_query:str, list_groups:list[str],size_table:int):
     for _ in range(size_table):
@@ -45,6 +52,16 @@ def get_teachers_id(c:Cursor)->list[int]:
     rows = c.fetchall()
     return [row[0] for row in rows]
 
+def get_students_id(c:Cursor)->list[int]:
+    c.execute('SELECT id From Students')
+    rows = c.fetchall()
+    return [row[0] for row in rows]
+
+def get_subjects_id(c:Cursor)->list[int]:
+    c.execute('SELECT id From Subjects')
+    rows = c.fetchall()
+    return [row[0] for row in rows]
+
 def fill_table_teachers(c:Cursor,sql_query:str, size_table:int):
     for _ in range(size_table):
         c.execute(sql_query, create_random_teacher())
@@ -57,11 +74,19 @@ def fill_table_subject(c:Cursor, sql_query:str, size_table:int):
         teachers_id = choice(teachers_ids)
         c.execute(sql_query, (subject,teachers_id))
 
+def fill_table_grade(c:Cursor, sql_query:str, size_table:int=20):
+    student_ids = get_students_id(c)
+    subjects_ids = get_subjects_id(c)
+    for student_id in student_ids:
+        for _ in range(size_table):
+            subjects_id = choice(subjects_ids)
+            c.execute(sql_query, (student_id,subjects_id, create_random_grade(),create_random_data()))
+
 if __name__ == '__main__':
 
     with MyConnection() as db:
         cursor = db.get_cursor()
-        fill_table_subject(cursor, form_subject, 8)
+        fill_table_grade(cursor, form_grade)
 
 
 
